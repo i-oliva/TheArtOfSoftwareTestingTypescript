7d84cbf0bf54a8277fd0f3ebae2e4c9b54a8b32c
Ismael Oliva <ismael.oliva@digiseq.co.uk>
2019-12-28 12:10:24 +0000

    TS and Jest works

---
+++ 1SelfAssesmentTest.ts
@@ -0,0 +1,4 @@
+// The program reads three integer values from an input dialog. The
+// three values represent the lengths of the sides of a triangle. The pro-
+// gram displays a message that states whether the triangle is scalene,
+// isosceles, or equilateral.
